d828a2545ae129cf943049a294bc64e5f2c22614
skinify <gabrielZK2000@hotmail.com>
2021-07-02 00:02:14 -0300

    Funcionando é o importante

--- PwBasicBot/PwBasicBot/Configs/ConfConstants.cs
+++ PwBasicBot/PwBasicBot/Configs/ConfConstants.cs
@@ -4,8 +4,7 @@ namespace PwBasicBot.Configs
 {
     public class ConfConstants
     {
-        public static readonly KeyBindingSection keyBindingConfig = ConfigurationManager.GetSection("KeyBinding") as KeyBindingSection;
-        public static readonly TempAddressSection tempAddressConfig = ConfigurationManager.GetSection("TempAddress") as TempAddressSection;
+        public static readonly BindingSection keyBindingConfig = ConfigurationManager.GetSection("Binding") as BindingSection;        
         public static readonly MacroSection macroConfig = ConfigurationManager.GetSection("Macro") as MacroSection;
         public static readonly TmpAdressSection tmpAddressConfig = ConfigurationManager.GetSection("TmpAdress") as TmpAdressSection;
     }
